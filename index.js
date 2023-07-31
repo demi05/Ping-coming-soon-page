ebee455496838617729e683be28840b6410e4a7e
Leshi Taiwo <98643006+demi05@users.noreply.github.com>
2023-07-31 15:29:09 +0100

    Commiting styles without validation

---
+++ index.js
@@ -0,0 +1,8 @@
+function doCustomValidation(){
+  var frm=document.forms["myForm"];
+  if (condition) {
+    
+  }
+}
+// var formValidator= new Validator("myForm");
+// formValidator.addValidation("Please provide a valid email address")
